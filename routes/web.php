2adb9e2a66a3d628daa74a9c0f1798ecd0ed385b
Ramyyy2 <ramyyros123@gmail.com>
2024-01-23 15:24:04 +0700

    abc

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/edit', function () {
 });
 });
 
-Route::get('/Dashboard', function () {
+Route::get('/', function () {
     return view('layouts.master');
 });
 Route::get('/Products', function () {
